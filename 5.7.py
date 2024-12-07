d970ccfaddfc0be82e4659bcb99f2483790ed97c
handeptrainhatthegioi <hovanhan115@gmail.com>
2024-12-07 20:46:12 +0700

    Add files via upload

--- 5.7.py
+++ 5.7.py
@@ -2,5 +2,5 @@ def Sequential_Search(dlist, item):
     """Tìm kiếm tuyến tính trong danh sách dlist."""
     for index, value in enumerate(dlist):
         if value == item:
-            return (True, index) 
-    return (False, -1) 
+            return (True, index)
+    return (False, -1)
